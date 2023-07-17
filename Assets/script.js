0000b92f3ea4f005e4820a6cf350fc628d900c09
PeterJanasz <petertjanasz@gmail.com>
2023-07-17 14:43:24 -0700

    added parseInt(timeNow) to get integer value from string

--- Assets/script.js
+++ Assets/script.js
@@ -8,14 +8,14 @@
 
 $(document).ready(function () {
 
-var currentDay = dayjs().format('dddd, MMMM D YYYY'); 
-var currentTime = dayjs().format('h:mm A')
+  var currentDay = dayjs().format('dddd, MMMM D YYYY');
+  var currentTime = dayjs().format('h:mm A')
 
-$("#currentDay").html(currentDay);
-$("#currentTime").html(currentTime);
-console.log(dayjs)
+  $("#currentDay").html(currentDay);
+  $("#currentTime").html(currentTime);
+  console.log(dayjs)
 
-  //Jquery onclick event 
+  //Jquery onclick event (A)
   $(".saveBtn").on("click", function () {
     //get value from textarea in html
     var text = $(this).siblings(".description").val();
@@ -32,19 +32,19 @@ console.log(dayjs)
     //can i use currentTime from global var?
     var timeNow = dayjs().hour();
 
-    //create array for current time to apply past, present, future class for each timeBlock
+    //create array for current time to apply past, present, future class for each timeBlock (B)
     $(".time-block").each(function () {
-        //take string value and return a number
-        //split hour id into an array
+      //take string value and return a number
+      //split hour id into an array
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
       //add class according to current time of day
-      if(blockTime < timeNow) {
+      if (blockTime < parseInt(timeNow)) {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
       }
-      else if(blockTime === timeNow) {
+      else if (blockTime === parseInt(timeNow)) {
         $(this).removeClass("future");
         $(this).addClass("present");
         $(this).removeClass("past");
@@ -55,18 +55,18 @@ console.log(dayjs)
         $(this).removeClass("past");
       }
 
-})
-    }
-    timeTracker();
-  });
-  // TODO: Add a listener for click events on the save button. This code should
+    })
+  }
+  timeTracker();
+});
+  // A) TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
+  // B) TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
